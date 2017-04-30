36cb38cf4d46f0b7381b62951dfdd8618cd705a9
Javal Nanda <javalnanda@gmail.com>
2017-05-01 01:30:55 +0530

    Add mapping logic

---
+++ AlamofireObjectMapperSample/AlamofireObjectMapperSample/Project.swift
@@ -0,0 +1,37 @@
+//  ViewController.swift
+//  AlamofireObjectMapperSample
+//
+//  Created by Javal Nanda on 5/1/17.
+//  Copyright © 2017 Javal Nanda. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Project: NSObject, Mappable {
+
+	var projectId: Int!
+    var accountId: Int!
+	var dateCreated: Int!
+	var dateModified: Int!
+	var name: String!
+	var status: String!
+
+    override init() {
+        super.init()
+    }
+
+    convenience required init?(map: Map) {
+        self.init()
+    }
+
+    func mapping(map: Map) {
+        projectId <- map["id"]
+        accountId <- map["account_id"]
+        dateCreated <- map["date_created"]
+        dateModified <- map["date_modified"]
+        name <- map["name"]
+        status <- map["status"]
+    }
+
+}
--- AlamofireObjectMapperSample/AlamofireObjectMapperSample/ViewController.swift
+++ AlamofireObjectMapperSample/AlamofireObjectMapperSample/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
 
 class ViewController: UIViewController {
-
+    
+    var projects:[Project] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fetchData()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func fetchData(){
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let apiUrl = "https://raw.githubusercontent.com/javalnanda/AlamofireObjectMapperSample/master/AOMsample.json"
+        Alamofire.request(apiUrl).validate().responseArray(keyPath: "result") { (response: DataResponse<[Project]>) in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            switch response.result {
+            case .success:
+                self.projects = response.result.value ?? []
+                for project in self.projects {
+                    print(project.name)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
